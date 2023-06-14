927998f5d695d3f6151ede245b3904abd9de73f3
Lavanya2227 <136583131+Lavanya2227@users.noreply.github.com>
2023-06-14 15:21:29 +0530

    Add files via upload

---
+++ Assignment-1.py
@@ -0,0 +1,6 @@
+# Fibonacci sequence
+
+i,j = 0,1
+while j < 50:
+    print(j)
+    i,j = j, i+j
---
+++ Assignment-1b.py
@@ -0,0 +1,6 @@
+# Word to mirror dimension
+
+word = "Edyoda"
+reverse_word = word[::-1]
+print(reverse_word)
+
